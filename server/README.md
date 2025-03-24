6be2dba9eab5d050063d288120a3bf67dd286a02
Andrew Diles <a_diles@hotmail.com>
2025-03-24 16:50:29 -0400

    initial state of project

--- server/README.md
+++ server/README.md
@@ -1,3 +1,5 @@
 # Backend
 
+Make your package.json, add your dependencies, etc
+
 **OPTIONAL: You can document your endpoints in this file.**
--- server/index.js
+++ server/index.js
@@ -1,29 +1,15 @@
-'use strict';
-
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
 const PORT = 4000;
 
-express()
-  .use(function(req, res, next) {
-    res.header(
-      'Access-Control-Allow-Methods',
-      'OPTIONS, HEAD, GET, PUT, POST, DELETE'
-    );
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-  })
-  .use(morgan('tiny'))
-  .use(express.static('./server/assets'))
-  .use(express.json())
-  .use(express.urlencoded({ extended: false }))
-  .use('/', express.static(__dirname + '/'))
+const app = express();
 
-  // REST endpoints?
-  .get('/bacon', (req, res) => res.status(200).json('🥓'))
+app.use(morgan("tiny"));
+app.use(express.json());
 
-  .listen(PORT, () => console.info(`Listening on port ${PORT}`));
+app.get("/test", (req, res) => res.status(200).json("🥓"));
+
+// endpoints go here
+
+app.listen(PORT, () => console.info(`Listening on port ${PORT}`));
