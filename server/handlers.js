16090acc4ca1fb034cdba7391d04de3a98be313d
MahanNilipour <mahannilipour@gmail.com>
2025-03-25 17:33:40 -0400

    my work

--- server/handlers.js
+++ server/handlers.js
@@ -2,9 +2,11 @@ const getItems = require("./handlers/getItems");
 const getSingleItem = require("./handlers/getItemId")
 const getCompagnies = require("./handlers/getCompagnies")
 const getCompany = require("./handlers/getCompany")
+const getCartId = require("./handlers/getCartId")
 module.exports = {
     getItems,
     getSingleItem,
     getCompagnies,
-    getCompany
+    getCompany,
+    getCartId
 };
