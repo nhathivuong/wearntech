4750cab7b5f21257c4d7efc5995ed5dd86542d94
Valerie Payeur <valeriepayeur@gmail.com>
2025-03-26 17:08:42 -0400

    valerie-css-fix-and-homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,15 +5,30 @@ import ItemCard from "./ItemCard"
 const Home = () => {
     // Getting all items from context
     const {allItems} = useContext(AllItemsContext)
-    console.log(allItems)
+    const selectedItems = []
+
+    // Getting a set number of random items from allItems context
+    if(allItems !==null){
+        const shuffled = allItems.sort(() => 0.5-Math.random())
+        const selected = shuffled.slice(0, 6)
+        selected.forEach(item => {
+            selectedItems.push(item)
+        })
+    }
+    
     return <>
     <div className="homepage">
         <h1>Wear & Tech</h1>
         <h2>Your one stop shop for wearable tech!</h2>
         <div className="featuredItems">
             <p>Featured items</p>
-            {allItems === null ? <p>Loading...</p> : <>
-            </>}
+            <div className="individualFeaturedItems">
+                {allItems === null ? <p>Loading...</p> : <>
+                {selectedItems.map((selectedItem) => {
+                    return <ItemCard key={selectedItem._id} item={selectedItem} />
+                })}
+                </>}
+            </div>
         </div>
     </div>
     </>
