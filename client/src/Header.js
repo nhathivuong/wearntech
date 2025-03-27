cddec1c0caf7a63e3db4d12bd1cb786a1f1e3870
Nha Thi Vuong <nhathivuong@gmail.com>
2025-03-26 21:51:29 -0400

    nav companies

--- client/src/Header.js
+++ client/src/Header.js
@@ -2,20 +2,35 @@ import { HomeIcon, CartIcon, SearchIcon } from "./components/Icons"
 import { NavLink } from "react-router-dom"
 import styled from "styled-components"
 import NavBarCompanies from "./NavBarCompanies"
+import { useState } from "react"
 
 const Header = () => {
-    return <StyledNavigationBar>
+    const [coNavOpen, setCoNavOpen] = useState(false)
+    const [itemNavOpen, setItemNavOpen] = useState(false)
+
+    const handleCoNav = () =>{
+        setCoNavOpen(!coNavOpen)
+    }
+    const handleItemNav = () =>{
+        setItemNavOpen(!itemNavOpen)
+    }
+    const closeNav = () =>{
+        setCoNavOpen(false);
+        setItemNavOpen(false)
+    }
+    return <><StyledNavigationBar>
         <div className="pagesNavigation">
-            <NavLink to="/"><HomeIcon /> Home</NavLink>
-            <NavLink to="/items">Items</NavLink>
-            <NavLink to="/companies">Brands</NavLink>
-            <NavBarCompanies/>
+            <NavLink to="/" onClick={closeNav}><HomeIcon/> Home</NavLink>
+            <NavLink onClick={handleItemNav}>Items</NavLink>
+            <NavLink onClick={handleCoNav}>Brands</NavLink>
         </div>
         <div className="searchAndCart">
-            <SearchIcon /> <input type="text" />
-            <NavLink to="/cart"><CartIcon /></NavLink>
+            <SearchIcon /> <input type="text" onClick={closeNav}/>
+            <NavLink to="/cart"  onClick={closeNav}><CartIcon /></NavLink>
         </div>
     </StyledNavigationBar>
+    {coNavOpen && <NavBarCompanies handleCoNav={handleCoNav}/>}
+    </>
 }
 
 
@@ -27,10 +42,14 @@ const StyledNavigationBar = styled.nav`
     display: flex;
     top: 0;
     width: 100%;
+    height:25px;
     padding: 1.5rem;
     position: sticky;
     margin: 0;
     padding: 1rem 0;
+    a{
+        color: var(--color-white);
+    }
     .pagesNavigation {
         display: inline-block;
         align-items: center;
@@ -39,12 +58,12 @@ const StyledNavigationBar = styled.nav`
         position: relative;
     }
     .searchAndCart{
-        display: inline-block;
+        display:flex;
+        align-items:center;
         margin-left: auto;
         position: absolute;
         right: 0;
         margin: 0 2rem;
 }
 `
-
 export default Header
--- client/src/NavBarCompanies.js
+++ client/src/NavBarCompanies.js
@@ -1,23 +1,40 @@
-import { useContext } from "react"
-import { NavLink } from "react-router-dom"
-import { AllItemsContext } from "./contexts/AllItemsContext"
+import { useContext} from "react"
 import {AllCompaniesContext} from "./contexts/AllCompaniesContext"
-const NavBarCompanies = () => {
-    const {compagnies} = useContext(AllCompaniesContext)
+import { NavLink } from "react-router-dom"
+import styled from "styled-components"
 
-    console.log(compagnies)
-    if(!compagnies){
-        
+const NavBarCompanies = ({handleCoNav}) => {
+    const {companies} = useContext(AllCompaniesContext)
+    if(!companies){
+        return <p>Loading companies...</p>
     }
-    // console.log(compagnies)
-    // if(!compagnies){
-    //     return <p>Loading companies...</p>
-    // }
-    return <p>companies</p>
-    // return(<>{
-    //     compagnies.map(company => {
-    //         return <NavLink to="/company/:_id">{company.name}</NavLink>
-    //     })
-    // }</>)
+    const companiesinAlphaOrder = companies.sort((a,b)=> a.name.localeCompare(b.name))
+
+    return(
+    <NavDrop>
+    {companiesinAlphaOrder.map(company => {
+        return <EachCompany key={company._id} to={`/company/${company._id}`} onClick={handleCoNav}>{company.name}</EachCompany>
+        })
+    }
+    </NavDrop>
+    )
 }
+const NavDrop = styled.div`
+    position:absolute;
+    box-sizing: border-box;
+    padding: 1em 0 1em 1em;
+    display:grid;
+    grid-template-rows: repeat(13, auto);
+    grid-template-columns: repeat(6, 1fr);
+    gap: 5px;
+    grid-auto-flow:column;
+    width:100%;
+    background-color: var(--color-yellow);
+`
+const EachCompany = styled(NavLink)`
+    height:fit-content;
+    width:fit-content;
+    padding: 0;
+    text-align:left;
+`  
 export default NavBarCompanies
