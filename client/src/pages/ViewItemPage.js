765d52528bc2fb89b00e2b3a8ab8f06fde62795c
az1v <aziz22.moe@gmail.com>
2025-03-28 11:19:39 -0400

    aziz

--- client/src/pages/ViewItemPage.js
+++ client/src/pages/ViewItemPage.js
@@ -1,154 +1,123 @@
 import { useParams, Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { CartContext } from "../contexts/CartContext"
 
-
-// display one product and its details
 const ViewItemPage = () => {
-  const params = useParams();
+  const { _id: itemId } = useParams();
   const [product, setProduct] = useState(null);
   const [company, setCompany] = useState(null);
-  const [fetched, setFetched] = useState(false);
   const [quantity, setQuantity] = useState(1);
-  const [error, setError] = useState(null);
-  const [status, setStatus] = useState(null);
+  const [status, setStatus] = useState("");
+  const [addedToCart, setAddedToCart] = useState(false);  //confirm that item was added to cart
 
-  // fetching the product details and the company details
+  const { _id: cartId } = useContext(CartContext); 
+
+  //Fetches
   useEffect(() => {
-    let mounted = true;
-
     const fetchItemData = async () => {
-      const response = await fetch(`/item/${params.itemId}`);
-      const data = await response.json();
-      if (data.status === 400 || data.status === 500) {
-        throw new Error(data.message);
-      } else {
-        if (mounted) {
-          setProduct(data.data);
-          await fetchCompanyData(data.data.companyId);
-        }
-      }
-    };
+      try {
+        //Fetch item's data from database
+        const itemResponse = await fetch(`/item/${itemId}`);
+        if (!itemResponse.ok) throw new Error("Could not fetch item");
+        const { itemData } = await itemResponse.json();
+        setProduct(itemData);
 
-    const fetchCompanyData = async (companyId) => {
-      const response = await fetch(`/company/${companyId}`);
-      const data = await response.json();
-      if (data.status === 400 || data.status === 500) {
-        throw new Error(data.message);
-      } else {
-        if (mounted) {
-          setCompany(data.data);
-        }
+        //Using item's data, fetch company data from database
+        const companyResponse = await fetch(`/company/${itemData.companyId}`);
+        if (!companyResponse.ok) throw new Error ("Could not fetch company");
+        const { companyData } = await companyResponse.json();
+        setCompany(companyData);
+      } catch (err) {
+        console.log(err.message);
       }
-    };
-
+    }
     fetchItemData();
+  },[itemId]);
+  
 
-    return () => {
-      mounted = false;
-    };
-  }, [params.itemId]);
-
-  const handleAddToCart = () => {
-    setFetched(true);
-    setStatus(null);
-    setError(null);
-
-    fetch("/add-item-to-cart", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        _id: product._id,
-        quantity: quantity,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.status === 400 || data.status === 500) {
-          setError(data.message);
-
-          throw new Error(data.message);
-        } else {
-          setStatus("Added to cart!");
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        setFetched(false);
-      });
+  const handleAddToCart = async (ev) => {
+    try {
+      ev.preventDefault();
+      setStatus("processing")
+      const orderData = {
+        _id: itemId,
+        quantity
+      }
+      const body = JSON.stringify( orderData );
+      const options = {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+        },
+        body
+      };
+      const addToCartResponse = await fetch(`/cart/${cartId}/${itemId}`, options); 
+      const { addToCartData } = await addToCartResponse.json();
+      if (addToCartData.status !== 201) {
+        setStatus("");
+        console.log(addToCartData.message);
+      } else {
+        setAddedToCart(true);
+      }
+      
+    } catch (error) {
+      console.log(error.message);
+    }
   };
+
+  if (!product || !company) {
+    return <p>Loading Information...</p>;
+  }
 
   return (
     <>
-      {!product || !company ? (
-        <Loading>Loading...</Loading>
-      ) : (
-        <Container>
-          <StatusBox>
-            {error && <Error>{error}</Error>}
-            {status && <Status>{status}</Status>}
-          </StatusBox>
-          <ProductBox>
-            <Image src={product.imageSrc} alt="productImg" />
-            <DetailsBox>
-              <ProductCategory>{product.category}</ProductCategory>
-              <ProductName>{product.name}</ProductName>
-              <div>
-                <Price>{product.price}</Price>
-                <Info>
-                  <div>
-                    <Details>Details</Details>
-                    <DetailsList>
-                      Designed by{" "}
-                      <StyledLink to={`/company/${company._id}`}>
-                        {company.name}
-                      </StyledLink>
-                      <Location>Used on: {product.body_location}</Location>
-                    </DetailsList>
-                  </div>
-                </Info>
+      {/* General Item Data */}
+      <section>
+        <img src={product.imageSrc} alt={product.name} />
+        <p>{product.category}</p>
+        <p>{product.name}</p>
+        <p>{product.price}</p>
+        <p>Designed By:</p>
+        <Link to={`/company/${company._id}`}>{company.name}</Link>
+        <p>Used on: {product.body_location}</p>
+      </section>
 
-                {product.numInStock === 0 ? (
-                  <DisabledButton>OUT OF STOCK</DisabledButton>
-                ) : (
-                  <Flex>
-                    <Quantity>
-                      <QuantButtonContainer>
-                        <QuantButton
-                          onClick={() => setQuantity(Math.max(quantity - 1, 1))}
-                          disabled={quantity === 1}
-                        >
-                          -
-                        </QuantButton>
-                        <QuantityValue>{quantity}</QuantityValue>
-                        <QuantButton
-                          onClick={() =>
-                            setQuantity(
-                              Math.min(quantity + 1, product.numInStock)
-                            )
-                          }
-                          disabled={quantity === product.numInStock}
-                        >
-                          +
-                        </QuantButton>
-                      </QuantButtonContainer>
-                      {quantity >= product.numInStock && (
-                        <MessageQuant>Maximum amount in stock!</MessageQuant>
-                      )}
-                    </Quantity>
+      {/* Stock and Quantity */}
+      <section>
+      {
+        product.numInStock === 0 ? (
+          <button disabled>OUT OF STOCK</button>
+        ) : (
+          <>
+            {/* Subtract quantity button */}
+            <button onClick={() => setQuantity(Math.max(quantity - 1, 1))} disabled={quantity === 1}> - </button>
+            {/* Displays desired amount*/}
+            <span>{quantity}</span>
+            {/* Increase quantity button */}
+            <button onClick={() => setQuantity(Math.min(quantity + 1, product.numInStock))} disabled={quantity === product.numInStock}> + </button>
 
-                    <Button onClick={handleAddToCart}>ADD TO CART</Button>
-                  </Flex>
-                )}
-              </div>
-            </DetailsBox>
-          </ProductBox>
-        </Container>
-      )}
+            {/* When max quantity is met, user is informed that they cannot purchase more*/}
+            { 
+              quantity === product.numInStock ? (
+                <p>Maximum quantity available. No more stock for sale...</p>
+              ) : (
+                <p>Please purchase as many as you'd like!</p>
+              )
+            }
+            <button onClick={handleAddToCart} disabled={status === "processing"}>Add to Cart</button>
+          </>
+        )}
+      </section>
+
+      {/* Confirmation message that item was added to cart*/}
+      <section>
+        {
+          addedToCart && <p>{quantity}x {product.name}: Successfully added to Cart!</p>
+        }
+      </section>
     </>
-  );
+  )
 };
 
 export default ViewItemPage;
